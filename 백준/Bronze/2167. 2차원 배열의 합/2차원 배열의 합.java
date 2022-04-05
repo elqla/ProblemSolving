44fea6635eb816d01d1d8e5daff5bb96a5367e1b
dabeen <97648343+elqla@users.noreply.github.com>
2022-04-05 22:03:43 +0900

    [Bronze I] Title: 2차원 배열의 합, Time: 2460 ms, Memory: 153912 KB -BaekjoonHub

---
+++ 백준/Bronze/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N, M, k, i, j, x, y;
+		int [][] arr;
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[300][300];
+		for (int a=0; a<N; a++) {
+			for (int b=0; b<M; b++) {
+				int num = sc.nextInt();
+				arr[a][b] = num;
+			}
+			
+		}
+		k = sc.nextInt();
+		for(int c = 0; c<k; c++) {
+			i = sc.nextInt();
+			j = sc.nextInt();
+			x = sc.nextInt();
+			y = sc.nextInt();
+			
+			int s = 0;
+			for(int d=i-1; d<x; d++) {
+				for(int e=j-1; e<y; e++) {
+					s += arr[d][e];
+				}
+			}System.out.println(s);
+			
+			
+		}
+		
+		
+		
+		
+	}//sc.close();
+	
+}
