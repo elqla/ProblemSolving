6b99e23b546d6c23d20acd3a91f176f8cdd16342
dabeen <97648343+elqla@users.noreply.github.com>
2022-04-19 23:42:59 +0900

    [Silver III] Title: 1로 만들기, Time: 612 ms, Memory: 38652 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,16 +1,18 @@
-from collections import defaultdict
-
-def makeone(n):
-    dp = defaultdict(int)
-    if n <= 1:
-        return 0
-    else:
-        for i in range(2, n+1):  # dp[1]은 연산할게 없어서 0 이므로 2부터 진행해준다.
-            dp[i] = dp[i-1] +1   #dp[i-1] = dp[i]에서 -1을 적용하는 연산을 하나 추가해줄 수 있다.
-            if i % 3 == 0:
-                dp[i] = min(dp[i], dp[i // 3] + 1)  # dp[i//3]+ 1 은 예를 들어 dp[9]는 dp[9//3]에서 한번만 더 연산을 해주면 되기 때문
-            if i % 2 == 0:
-                dp[i] = min(dp[i], dp[i // 2] + 1)  # dp[6] 은 dp[6//3]-> 6/3=2 , 2/2=1  (dp[2]+1)
-        return dp[i]
 n = int(input())
-print(makeone(n))
+
+dp = [0]*(n+4)
+dp[1] = 0  # idx 숫자로 맞춰줌
+dp[2] = 1
+dp[3] = 1
+
+if n<4:
+    print(dp[n])
+else:
+    for i in range(4, n+1):
+        dp[i] = dp[i - 1] + 1
+        if i%3==0:
+            dp[i] = min(dp[i], dp[i//3]+1)
+        if i%2==0:
+            dp[i] = min(dp[i], dp[i//2]+1)
+            
+    print(dp[n])
