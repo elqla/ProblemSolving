929399ca7cfa0ba488994e760c1fbe535d73aea7
dabeen <97648343+elqla@users.noreply.github.com>
2022-11-28 10:06:40 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,18 +1,9 @@
-# import sys
-# sys.stdin = open('input.txt', 'r')
-
-cnt = 0
-for t in range(int(input())):
-    input_lst = list(input())
-    lst = [input_lst[0]]
+t = int(input())
+cnt = t
+for t in range(t):
+    input_lst = input()
     for i in range(1, len(input_lst)):
-        if lst[i-1] == input_lst[i]:
-            lst.append(input_lst[i])
-        else:
-            if input_lst[i] in lst:
-                break
-            else:
-                lst.append(input_lst[i])
-    if(len(input_lst)) == (len(lst)):
-        cnt += 1
+        if(input_lst[i-1] != input_lst[i] and input_lst[i] in input_lst[:i]):
+            cnt -= 1
+            break
 print(cnt)
