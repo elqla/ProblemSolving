773d04526f9bc7e101dc88b4785d354f60af7238
dabeen <97648343+elqla@users.noreply.github.com>
2022-11-28 10:49:44 +0900

    [Silver V] Title: 소수 찾기, Time: 72 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,11 +1,11 @@
+import math
 
 def is_prime(n):
     if(n==1): return False
-    for i in range(2, n//2+1):  #절반 이상의 수는 필요없음
-        if n%i == 0:
-            return False #i로 나누어 떨어지면 소수가 아님
+    for i in range(2, int(math.sqrt(n))+1): #n의 제곱근을 정수화한 후 + 1
+        if n % i == 0:
+            return False
     return True
-
 
 # 소수: 1을 제외한 수 중, 1과 자기 자신만을 약수로 가지는 수
 t = int(input())
